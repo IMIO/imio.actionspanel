0781d8d0f9c0b3ed47a22ef0396fe94b89d29927
Gauthier Bastien <gauthier.bastien@imio.be>
2013-11-27 10:42:51 +0000

    Added imio.actions_panel MessageFactory

--- src/imio/actionspanel/__init__.py
+++ src/imio/actionspanel/__init__.py
@@ -1,1 +1,10 @@
-#i am a python package
+# -*- coding: utf-8 -*-
+"""Init and utils."""
+
+from zope.i18nmessageid import MessageFactory
+
+ActionsPanelMessageFactory = MessageFactory('imio.actionspanel')
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
