5ed3af6009bd4c0f52751ca4572ec24a10eee03c
Gauthier Bastien <gauthier.bastien@imio.be>
2013-12-02 13:40:33 +0000

    Wait for document to be ready

--- src/imio/actionspanel/browser/static/actionspanel.js
+++ src/imio/actionspanel/browser/static/actionspanel.js
@@ -1,7 +1,11 @@
-jQuery(function($) {
-  // Add transition confirmation popup
-  $('a.link-overlay-actionspanel.transition-overlay').prepOverlay({
-        subtype: 'ajax',
-        closeselector: '[name="form.buttons.cancel"]',
-  });
-});
+initializeTransitionsOverlays = function () {
+    jQuery(function($) {
+      // Add transition confirmation popup
+      $('a.link-overlay-actionspanel.transition-overlay').prepOverlay({
+            subtype: 'ajax',
+            closeselector: '[name="form.buttons.cancel"]',
+      });
+    });
+};
+
+jQuery(document).ready(initializeTransitionsOverlays);
