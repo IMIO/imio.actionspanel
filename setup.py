8f428db132cef621f7f233007e724d8d9c8af4cb
Simon Delcourt <delcourt.simon@gmail.com>
2014-08-20 13:25:50 +0200

    Back to development: 1.6

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='imio.actionspanel',
-    version='1.5',
+    version='1.6.dev0',
     description="Actions panel",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
