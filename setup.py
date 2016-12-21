501bebc39f0cc5c51566f00f16a9aa7457f78612
Gauthier Bastien <g.bastien@imio.be>
2016-12-21 11:04:14 +0100

    Back to development: 1.22

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='imio.actionspanel',
-    version='1.21',
+    version='1.22.dev0',
     description="Actions panel",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
