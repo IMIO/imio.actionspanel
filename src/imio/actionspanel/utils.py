8f3dabb7bf51844b8367b65ea548d9e6dac20bc7
Gauthier Bastien <g.bastien@imio.be>
2019-04-08 14:58:40 +0200

    Typo

--- src/imio/actionspanel/utils.py
+++ src/imio/actionspanel/utils.py
@@ -7,10 +7,10 @@ from Products.CMFCore.permissions import View
 
 def unrestrictedRemoveGivenObject(object_to_delete):
     """
-      This method removed the given object  view removes a given object but as a Manager,
+      This method removes a given object but as a Manager,
       so calling it will have relevant permissions.
       This is done to workaround a strange Zope behaviour where to remove an object,
-      the user must have the 'Delete objects' permission on the parent wich is not always easy
+      the user must have the 'Delete objects' permission on the parent which is not always easy
       to handle.  This is called by the 'remove_givenuid' view that does the checks if user
       has at least the 'Delete objects' permission on the p_object_to_delete.
     """
